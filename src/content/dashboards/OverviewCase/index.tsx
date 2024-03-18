b3a308b079114ca5af7a6cf56d8995fd674ee458
eddie wang <wang8119@gmail.com>
2024-03-18 23:15:24 +0800

    reducer action initstate export

--- src/content/dashboards/OverviewCase/index.tsx
+++ src/content/dashboards/OverviewCase/index.tsx
@@ -1,81 +1,15 @@
-import { useReducer, ChangeEvent } from 'react';
-import useProjectListsQuery from '../../../hooks/useProjectListsQuery';
-
-import projectFilterReducer from '../../../reducers/projectFilterReducer';
 import ProjectListPanel from '../../../components/ProjectListPanel';
-
-import {
-  MenuItem,
-  InputLabel,
-  FormControl,
-  Select,
-  TextField,
-} from '@mui/material';
-
+import ProjectSelect from '../../../components/ProjectSelect';
 import PageTitle from './../../../components/PageTitle';
 import CreateProject from './CreateProject';
 
 const OverviewCase = () => {
-  const DEFAULT_PAGINATION = {
-    current: 1,
-    pageSize: 6,
-    showSizeChanger: true,
-    pageSizeOptions: [3, 6, 8],
-  };
-
-  const INITIAL_STATE = {
-    filter: { keyword: '', category: 'All' },
-    pagination: DEFAULT_PAGINATION,
-  };
-
-  const [variables, dispatch] = useReducer(projectFilterReducer, INITIAL_STATE);
-  const { projectListsdata } = useProjectListsQuery({ variables });
-
-  const handleChangeCategory = (event: ChangeEvent<{ value: string }>) => {
-    dispatch({
-      type: 'CHANGE_CATEGORY',
-      payload: { category: event.target.value },
-    });
-  };
-
-  const handleChangeKeyword = (event: ChangeEvent<{ value: string }>) => {
-    dispatch({
-      type: 'CHANGE_KEYWORD',
-      payload: { keyword: event.target.value },
-    });
-  };
-
   return (
     <div className="flex h-full w-full flex-col gap-6 p-6">
       <PageTitle title="Project List" />
       <CreateProject />
-      {/* project select area */}
-      <div className="flex items-center">
-        <p className="text-black">Projeect Select</p>
-        <FormControl sx={{ m: 1, width: 150 }}>
-          <InputLabel id="demo-simple-select-label">category</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            label="category"
-            value={variables.filter.category}
-            onChange={handleChangeCategory}
-          >
-            <MenuItem value="All">All</MenuItem>
-            <MenuItem value="House">House</MenuItem>
-            <MenuItem value="Mansion">Mansion</MenuItem>
-            <MenuItem value="Commercial">Commercial</MenuItem>
-            <MenuItem value="Office">Office</MenuItem>
-          </Select>
-        </FormControl>
-        <TextField
-          label="search name"
-          type="text"
-          value={variables.filter.keyword}
-          onChange={handleChangeKeyword}
-        />
-      </div>
-      <ProjectListPanel dataSource={projectListsdata} />
+      <ProjectSelect />
+      {/* <ProjectListPanel dataSource={projectListsdata} /> */}
     </div>
   );
 };
--- src/reducers/projectFilterReducer.tsx
+++ src/reducers/projectFilterReducer.tsx
@@ -1,25 +1,44 @@
 // import { isUndefined } from 'lodash';
 
+const DEFAULT_PAGINATION = {
+  current: 1,
+  pageSize: 6,
+  showSizeChanger: true,
+  pageSizeOptions: [3, 6, 8],
+};
+
+export const INITIAL_STATE = {
+  filter: { keyword: '', category: 'All' },
+  pagination: DEFAULT_PAGINATION,
+};
+
+export const ACTIONS = {
+  CHANGE_KEYWORD: 'CHANGE_KEYWORD',
+  CHANGE_CATEGORY: 'CHANGE_CATEGORY',
+  CHANGE_PAGINATION: 'CHANGE_PAGINATION',
+  CHANGE_SORT: 'CHANGE_SORT',
+};
+
 const projectFilterReducer = (state, action) => {
   switch (action.type) {
-    case 'CHANGE_KEYWORD':
+    case ACTIONS.CHANGE_KEYWORD:
       return {
         ...state,
         filter: { ...state.filter, keyword: action.payload.keyword },
         pagination: { ...state.pagination, current: 1 },
       };
-    case 'CHANGE_CATEGORY':
+    case ACTIONS.CHANGE_CATEGORY:
       return {
         ...state,
         filter: { ...state.filter, category: action.payload.category },
         pagination: { ...state.pagination, current: 1 },
       };
-    case 'CHANGE_PAGINATION':
+    case ACTIONS.CHANGE_PAGINATION:
       return {
         ...state,
         pagination: { ...state.pagination, ...action.payload.pagination },
       };
-    case 'CHANGE_SORT':
+    case ACTIONS.CHANGE_SORT:
       const newVariable = { ...state, sort: action.payload.sort };
       //   if (isUndefined(newVariable.sort.order)) delete newVariable['sort'];
       return newVariable;
