424ad91d77e4b019f748ccd6d7d5e6a1d64cd248
eddie wang <wang8119@gmail.com>
2024-03-20 23:38:02 +0800

    add mui Dialog

--- src/content/dashboards/OverviewCase/index.tsx
+++ src/content/dashboards/OverviewCase/index.tsx
@@ -1,15 +1,65 @@
+import { useState } from 'react';
 import ProjectListPanel from '../../../components/ProjectListPanel';
 import ProjectSelect from '../../../components/ProjectSelect';
 import PageTitle from './../../../components/PageTitle';
 import CreateProject from './CreateProject';
 import { ProjectListProvider } from '../../../context/ProjectListContext';
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+} from '@mui/material';
+
+import ClearIcon from '@mui/icons-material/Clear';
 
 const OverviewCase = () => {
+  const [openCreateProject, setOpenCreateProject] = useState<boolean>(false);
+
+  const handleCreateProjectOpen = () => {
+    setOpenCreateProject(true);
+  };
+
+  const handleCreateProjectClose = () => {
+    setOpenCreateProject(false);
+  };
+
   return (
     <ProjectListProvider>
       <div className="flex h-full w-full flex-col gap-6 p-6">
         <PageTitle title="Project List" />
-        <CreateProject />
+        <Button
+          variant="contained"
+          style={{ width: '200px' }}
+          onClick={handleCreateProjectOpen}
+        >
+          Create Project
+        </Button>
+
+        <Dialog
+          open={openCreateProject}
+          onClose={handleCreateProjectClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle>
+            Create Project{' '}
+            <IconButton
+              edge="end"
+              color="inherit"
+              onClick={handleCreateProjectClose}
+              aria-label="close"
+              style={{ position: 'absolute', right: '20px', top: '10px' }}
+            >
+              <ClearIcon />
+            </IconButton>
+          </DialogTitle>
+
+          <DialogContent>
+            <CreateProject />
+          </DialogContent>
+        </Dialog>
+
         <ProjectSelect />
         <ProjectListPanel />
       </div>
