c2ea9c60061744e80a1a39a7ea72b2a563d2c27f
eddie wang <wang8119@gmail.com>
2024-03-27 00:10:58 +0800

    create common typescript file

---
+++ src/types/common.d.ts
@@ -0,0 +1,10 @@
+interface ProjectData {
+    id: number;
+    name: string;
+    status: string;
+    date: string;
+    picture?: string;
+    fileNumber: string;
+    cost: number;
+    category: string;
+}
