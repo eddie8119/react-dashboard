647b62379535b542ca14a0ee3641a8589c20ef9d
eddie wang <wang8119@gmail.com>
2024-03-31 09:34:48 +0800

    FirmTaskPanel add cancel icon

--- src/content/dashboards/ProjectPlan/FirmTaskPanel.tsx
+++ src/content/dashboards/ProjectPlan/FirmTaskPanel.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import CreatTodo from './CreatTodo';
 import TodoListsArea from './TodoListsArea';
+import { IconButton } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
 
 interface FirmTaskPanelProps {
   firmTask: ThirdPartyData;
@@ -13,7 +15,22 @@ const FirmTaskPanel: FC<FirmTaskPanelProps> = ({ firmTask }) => {
       key={firmTask.id}
       className="flex w-[400px] flex-col gap-6 border border-black p-4  text-black"
     >
-      <h1 className="text-center">{firmTask.name}</h1>
+      <div className="relative flex items-center justify-center">
+        <h1>{firmTask.name}</h1>
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="close"
+          style={{
+            position: 'absolute',
+            right: '0px',
+            top: '0px',
+          }}
+        >
+          <ClearIcon />
+        </IconButton>
+      </div>
+
       <CreatTodo />
       <TodoListsArea firmTaskLists={firmTask.taskLists} />
     </div>
