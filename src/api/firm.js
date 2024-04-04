b04f7fe37a5fd1dacaf57e18e023e56ed7378744
eddie wang <wang8119@gmail.com>
2024-04-05 02:13:19 +0800

    整理api

--- src/api/firm.js
+++ src/api/firm.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
+const url = 'http://localhost:3000';
 
 //取得廠商列表
 export const getFirmLists = async () => {
-  return await axios.get('http://localhost:3000/firmLists');
+  return await axios.get(`${url}/firmLists`);
 };
--- src/api/project.js
+++ src/api/project.js
@@ -1,38 +1,39 @@
 import axios from 'axios';
+const url = 'http://localhost:3000';
 
 //取得專案類型列表
 export const getProjectTypeLists = async () => {
-  return await axios.get(`http://localhost:3000/projectTypeLists`);
+  return await axios.get(`${url}/projectTypeLists`);
 };
 
 //取得所有專案列表
 export const getProjectLists = async () => {
-  return await axios.get(`http://localhost:3000/projectLists`);
+  return await axios.get(`${url}/projectLists`);
 };
 
 //更新專案列表
 export const updateProject = async (data) => {
-  axios.post('http://localhost:3000/projectLists', data);
+  axios.post('${url}/projectLists', data);
 };
 
 //取得單一專案
 export const getProject = async (id) => {
-  return await axios.get(`http://localhost:3000/projectLists/${id}`);
+  return await axios.get(`${url}/projectLists/${id}`);
 };
 
 //刪除專案
 export const deleteProject = async (id) => {
-  axios.delete(`http://localhost:3000/projectLists/${id}`);
+  axios.delete(`${url}/projectLists/${id}`);
 };
 
 //編輯專案
 export const editProject = async (id, data) => {
-  await axios.put(`http://localhost:3000/projectLists/${id}`, data);
+  await axios.put(`${url}/projectLists/${id}`, data);
 };
 
 //更新專案協力廠商
 export const editProjectThirdParty = async (id, data) => {
-  await axios.patch(`http://localhost:3000/projectLists/${id}`, {
+  await axios.patch(`${url}/projectLists/${id}`, {
     thirdPartyLists: data,
   });
 };
