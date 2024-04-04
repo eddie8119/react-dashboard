da50012b23f50ef4155a7403525bd25ba15ba623
eddie wang <wang8119@gmail.com>
2024-04-05 00:45:27 +0800

    抽離 useProjectListsQuery hook 的api

--- src/api/project.js
+++ src/api/project.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+//取得所有專案列表
+export const getProjectLists = async () => {
+  return await axios.get(`http://localhost:3000/projectLists`);
+};
+
 //專案協力廠商
 export const editProjectThirdParty = async (projectId, data) => {
   await axios.patch(`http://localhost:3000/projectLists/${projectId}`, {
