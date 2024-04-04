506146a5a4afcfb9c3a8c6631fe8083ca9abc21f
eddie wang <wang8119@gmail.com>
2024-04-05 01:21:37 +0800

    抽離 getProjectTypeLists, editProject api

--- src/api/project.js
+++ src/api/project.js
@@ -10,24 +10,29 @@ export const getProjectLists = async () => {
   return await axios.get(`http://localhost:3000/projectLists`);
 };
 
-//取得單一專案
-export const getProject = async (id) => {
-  return await axios.get(`http://localhost:3000/projectLists/${id}`);
-};
-
-//刪除專案列表
-export const deleteProject = async (id) => {
-  axios.delete(`http://localhost:3000/projectLists/${id}`);
-};
-
 //更新專案列表
 export const updateProject = async (data) => {
   axios.post('http://localhost:3000/projectLists', data);
 };
 
+//取得單一專案
+export const getProject = async (id) => {
+  return await axios.get(`http://localhost:3000/projectLists/${id}`);
+};
+
+//刪除專案
+export const deleteProject = async (id) => {
+  axios.delete(`http://localhost:3000/projectLists/${id}`);
+};
+
+//編輯專案
+export const editProject = async (id, data) => {
+  await axios.put(`http://localhost:3000/projectLists/${id}`, data);
+};
+
 //專案協力廠商
-export const editProjectThirdParty = async (projectId, data) => {
-  await axios.patch(`http://localhost:3000/projectLists/${projectId}`, {
+export const editProjectThirdParty = async (id, data) => {
+  await axios.patch(`http://localhost:3000/projectLists/${id}`, {
     thirdPartyLists: data,
   });
 };
