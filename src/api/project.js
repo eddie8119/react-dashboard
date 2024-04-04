41632833c631b4a8d8a8efa13d26f0215c434a49
eddie wang <wang8119@gmail.com>
2024-04-05 00:57:10 +0800

    抽離 deleteProject api

--- src/api/project.js
+++ src/api/project.js
@@ -5,6 +5,11 @@ export const getProjectLists = async () => {
   return await axios.get(`http://localhost:3000/projectLists`);
 };
 
+//刪除專案列表
+export const deleteProject = async (id) => {
+  axios.delete(`http://localhost:3000/projectLists/${id}`);
+};
+
 //專案協力廠商
 export const editProjectThirdParty = async (projectId, data) => {
   await axios.patch(`http://localhost:3000/projectLists/${projectId}`, {
