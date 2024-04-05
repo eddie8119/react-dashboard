83c98cd189c9da9f4aba1366b7ad8446bd910dc5
eddie wang <wang8119@gmail.com>
2024-04-05 12:28:58 +0800

    TodoListsArea ui調整

--- src/content/dashboards/ProjectPlan/TodoListsArea.tsx
+++ src/content/dashboards/ProjectPlan/TodoListsArea.tsx
@@ -1,6 +1,7 @@
 import { useState, lazy, useContext, FC } from 'react';
 import ProjectContext from '../../../context/ProjectContext';
 import { editProjectThirdParty } from '../../../api/project';
+import Input from '@mui/material/Input';
 const PopUp = lazy(() => import('../../../components/PopUp'));
 
 interface TodoListsAreaProps {
@@ -66,23 +67,27 @@ const TodoListsArea: FC<TodoListsAreaProps> = ({
       {firmTaskLists.length !== 0 && (
         <div className=" flex items-center justify-between">
           <div className="flex gap-2">
-            <p className="w-[20px]"></p>
+            <p className="w-[20px]" />
             <p className="w-[90px]">Title</p>
             <p className="w-[60px]">Quantity</p>
-            <p className="w-[40px] text-center">Uint</p>
-            <p className="w-[40px] text-center">Cost</p>
-            <p className="w-[40px] text-center">Price</p>
+            <p className="w-[60px] text-center">Uint</p>
+            <p className="w-[120px] text-center">Cost</p>
           </div>
         </div>
       )}
       <div className="flex max-h-[250px] w-full flex-col gap-2 overflow-y-auto">
         {firmTaskLists.map((task, index) => (
           <div key={task.id} className=" flex items-center justify-between">
-            <div className="flex gap-2">
-              <p className="w-[20px]">{index + 1}</p>
-              <p className="w-[90px]">{task.todo}</p>
-              <p className="w-[60px] text-center">{task.quantity}</p>
-              <p className="w-[40px]">{task.uint}</p>
+            <div className="flex  gap-2">
+              <div className="flex w-[20px] items-center">{index + 1}</div>
+              <div className="flex w-[90px] items-center">{task.todo}</div>
+              <div className="flex w-[60px] items-center justify-center">
+                {task.quantity}
+              </div>
+              <div className="flex w-[60px] items-center justify-center">
+                {task.uint}
+              </div>
+              <Input placeholder="Cost" style={{ width: '100px' }} />
             </div>
 
             {/* 彈窗 */}
