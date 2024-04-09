eebe4d9acb45273ce2dacb9eddcea235b0d6adf2
eddie wang <wang8119@gmail.com>
2024-04-09 12:02:28 +0800

    移除不要的檔案

--- src/__tests__/createProject.test.tsx
+++
@@ -1,6 +0,0 @@
-import { render } from '@testing-library/react';
-import CreateProject from '../content/dashboards/OverviewCase/CreateProject';
-
-test('should create new project', async () => {
-  render(<CreateProject handleCreateProjectClose={() => {}} />);
-});
