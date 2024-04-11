efdf927133ad546221a682f7138baa1225d22c5e
eddie wang <wang8119@gmail.com>
2024-04-12 00:36:36 +0800

    利用apply抽出樣式

--- src/components/PageButtonPanel/index.tsx
+++ src/components/PageButtonPanel/index.tsx
@@ -54,7 +54,7 @@ const PageButtonPanel: FC<PageButtonPanelProps> = ({
           <button
             onClick={handlePopOpen}
             key={button.id}
-            className="rounded-md  border border-transparent bg-red-600 px-4 py-2 text-center text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+            className="btn-danger px-4 py-2"
           >
             {button.name}
           </button>
