1727266919d8285fd57c6de4bbdd1847fc7d189b
eddie wang <wang8119@gmail.com>
2024-04-22 23:44:06 +0800

    建立 context- FirmListCreateContext

--- src/content/dashboards/MaterialLibrary/FirmListPannel.tsx
+++ src/content/dashboards/MaterialLibrary/FirmListPannel.tsx
@@ -1,15 +1,42 @@
-import FirmList from '../../../components/FirmListUi';
+import { useState, useEffect } from 'react';
+import FirmListApi from './FirmListApi';
 import AddFirm from './AddFirm';
+import FirmListCreateContext, {
+  initFirmListCreateContext,
+} from '../../../context/FirmListCreateContext';
+import { getFirmLists } from '../../../api/firm';
 
 const FirmListPannel = () => {
+  const [updateFirmList, setUpdateFirmList] = useState<boolean>(
+    initFirmListCreateContext.updateFirmList,
+  );
+  const [firmLists, setFirmLists] = useState<FirmObject[]>([]);
+
+  const handlerSetUpdateFirmList = () => {
+    setUpdateFirmList(!updateFirmList);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getFirmLists();
+      setFirmLists(response.data);
+    };
+
+    fetchData();
+  }, [updateFirmList]);
+
   return (
-    <section className="container-box">
-      <header className="text-black">
-        <h1>Construction Type</h1>
-      </header>
-      <AddFirm />
-      <FirmList />
-    </section>
+    <FirmListCreateContext.Provider
+      value={{ updateFirmList, firmLists, handlerSetUpdateFirmList }}
+    >
+      <section className="container-box">
+        <header className="text-black">
+          <h1>Construction Type</h1>
+        </header>
+        <AddFirm />
+        <FirmListApi />
+      </section>
+    </FirmListCreateContext.Provider>
   );
 };
 
