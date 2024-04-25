c40917bea16a5a78d4619e0a53efb87583729168
eddie wang <wang8119@gmail.com>
2024-04-25 18:57:05 +0800

    將useGetFirmLists 增加 可選參數 - updateFirmList

--- src/hooks/useGetFirmLists.tsx
+++ src/hooks/useGetFirmLists.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { getFirmLists } from '../api/firm';
 
-const useGetFirmLists = () => {
+const useGetFirmLists = (updateFirmList?: boolean) => {
   const [firmLists, setFirmLists] = useState<FirmObject[]>([]);
 
   useEffect(() => {
@@ -11,7 +11,7 @@ const useGetFirmLists = () => {
     };
 
     fetchData();
-  }, []);
+  }, [updateFirmList]);
 
   return firmLists;
 };
