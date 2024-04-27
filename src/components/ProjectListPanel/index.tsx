2fbc2cf3f3c7966f852f65cf7e846efe7fcbd0fb
eddie wang <wang8119@gmail.com>
2024-04-27 23:08:30 +0800

    放置Pagination

--- src/components/ProjectListPanel/index.tsx
+++ src/components/ProjectListPanel/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ProjectListContext } from '../../context/ProjectListContext';
 import useDateHandler from '../../hooks/useDateHandler';
+import Pagination from '@mui/material/Pagination';
 
 const ProjectListPanel = () => {
   const { projectListsdata } = useContext(ProjectListContext);
@@ -13,36 +14,39 @@ const ProjectListPanel = () => {
   ];
 
   return (
-    <div className="grid w-full grid-cols-1 gap-3 md:grid-cols-3 ">
-      {projectListsdata.map((data) => (
-        <div
-          key={data.id}
-          className="container-box flex items-center justify-between bg-box-bg px-[30px]"
-        >
-          <div className="flex w-full items-center justify-between">
-            <div className="flex flex-col ">
-              <p className="text font-bold text-gray">
-                {data.fileNumber}-({data.category})
-              </p>
-              <p className="text text-primary">{data.name}</p>
-              <p className="text text-primary">{data.status}</p>
-              <p className="text text-primary">{useDateHandler(data.date)}</p>
-              <p className="text text-primary">cost: {data.cost}</p>
-            </div>
-            <div className="flex flex-col gap-2 ">
-              {buttonLists.map((button) => (
-                <Link
-                  key={button.id}
-                  to={`${button.link}/${data.id}`}
-                  className="rounded bg-blue-500 px-2 py-1 text-center text-white hover:bg-blue-700"
-                >
-                  {button.name}
-                </Link>
-              ))}
+    <div className="container-box flex h-[400px] w-full flex-col ">
+      <Pagination count={10} variant="outlined" color="primary" />
+      <div className="grid grid-cols-1 gap-3 md:grid-cols-3">
+        {projectListsdata.map((data) => (
+          <div
+            key={data.id}
+            className="container-box flex h-[150px] items-center justify-between bg-box-bg px-[30px]"
+          >
+            <div className="flex w-full items-center justify-between">
+              <div className="flex flex-col ">
+                <p className="text font-bold text-gray">
+                  {data.fileNumber}-({data.category})
+                </p>
+                <p className="text text-primary">{data.name}</p>
+                <p className="text text-primary">{data.status}</p>
+                <p className="text text-primary">{useDateHandler(data.date)}</p>
+                <p className="text text-primary">cost: {data.cost}</p>
+              </div>
+              <div className="flex flex-col gap-2 ">
+                {buttonLists.map((button) => (
+                  <Link
+                    key={button.id}
+                    to={`${button.link}/${data.id}`}
+                    className="rounded bg-blue-500 px-2 py-1 text-center text-white hover:bg-blue-700"
+                  >
+                    {button.name}
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
