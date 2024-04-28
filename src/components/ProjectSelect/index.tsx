2863a7ccafb8c5c9153ab372b9cf1baf5e5c1699
eddie wang <wang8119@gmail.com>
2024-04-28 11:04:03 +0800

    costSort 排序ui

--- src/components/ProjectSelect/index.tsx
+++ src/components/ProjectSelect/index.tsx
@@ -8,15 +8,21 @@ import {
   FormControl,
   Select,
   TextField,
+  Grid,
 } from '@mui/material';
+
 const ProjectSelect = ({
   projectTypeLists,
 }: {
   projectTypeLists: ProjectTypeObject[];
 }) => {
   const { t } = useTranslation();
-  const { variables, handleChangeCategory, handleChangeKeyword } =
-    useContext(ProjectListContext);
+  const {
+    variables,
+    handleChangeCategory,
+    handleChangeKeyword,
+    handleChangeCostSort,
+  } = useContext(ProjectListContext);
 
   const changeCategory = (event: SelectChangeEvent<string>): void => {
     handleChangeCategory(event.target.value);
@@ -24,37 +30,70 @@ const ProjectSelect = ({
   const changeKeyword = (event: ChangeEvent<{ value: string }>): void => {
     handleChangeKeyword(event.target.value);
   };
+  const changeCostSort = (event: SelectChangeEvent<string>): void => {
+    handleChangeCostSort(event.target.value);
+  };
   return (
-    <section className="flex items-center">
+    <Grid container spacing={1} alignItems="center">
       <header className="text-black">
         <h1>{t(`overviewCase.projectSelect.Projeect-Select`)}</h1>
       </header>
-      <FormControl sx={{ m: 1, width: 150 }}>
-        <InputLabel id="demo-simple-select-label">
-          {t('input-label.Category')}
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          label="category"
-          value={variables.filter.category}
-          onChange={(event) => changeCategory(event)}
-        >
-          <MenuItem value="All">{t('selection.project-select.All')}</MenuItem>
-          {projectTypeLists.map((item) => (
-            <MenuItem key={item.id} value={item.name}>
-              {t(`selection.project-select.${item.name}`)}
+
+      <Grid item>
+        <FormControl sx={{ width: 150 }}>
+          <InputLabel id="category-select-label">
+            {t('input-label.Category')}
+          </InputLabel>
+          <Select
+            labelId="category-select-label"
+            id="category-select"
+            label="category"
+            value={variables.filter.category}
+            onChange={(event) => changeCategory(event)}
+          >
+            <MenuItem value="All">{t('selection.project-select.All')}</MenuItem>
+            {projectTypeLists.map((item) => (
+              <MenuItem key={item.id} value={item.name}>
+                {t(`selection.project-select.${item.name}`)}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item>
+        <FormControl sx={{ width: 120 }}>
+          <InputLabel id="cost-order-select-label">
+            {t('input-label.Cost-Order')}
+          </InputLabel>
+          <Select
+            labelId="cost-order-select-label"
+            id="cost-order-select"
+            label="Cost"
+            value={variables.filter.costSort}
+            onChange={(event) => changeCostSort(event)}
+          >
+            <MenuItem value="">
+              {t('selection.project-select.No-Order')}
             </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <TextField
-        label={t('input-label.Search-Name')}
-        type="text"
-        value={variables.filter.keyword}
-        onChange={(event) => changeKeyword(event)}
-      />
-    </section>
+            <MenuItem value="ascending">
+              {t('selection.project-select.Ascending-Order')}
+            </MenuItem>
+            <MenuItem value="descending">
+              {t('selection.project-select.Descending-Order')}
+            </MenuItem>
+          </Select>
+        </FormControl>{' '}
+      </Grid>
+      <Grid item>
+        {' '}
+        <TextField
+          label={t('input-label.Search-Name')}
+          type="text"
+          value={variables.filter.keyword}
+          onChange={(event) => changeKeyword(event)}
+        />
+      </Grid>
+    </Grid>
   );
 };
 export default ProjectSelect;
